58e0b62bba96d577057f7a306bf9c17380fd689a
Ying Quan Tan <ying.quan.tan@gmail.com>
2017-05-12 15:28:40 -0400

    indentation

--- get_a_lunch_spot.py
+++ get_a_lunch_spot.py
@@ -3,27 +3,26 @@ from pprint import pprint
 from random import randrange
 
 class Restaurant:
-    def __init__(self, name, preferences):
-        self.name = name
-        self.preferences = preferences
+  def __init__(self, name, preferences):
+    self.name = name
+    self.preferences = preferences
 
     # checks for strict accomodation
-    def can_accomodate(self, desired):
-        for d in desired:
-            if d not in self.preferences:
-                # as long as one strict requirement cannot be met, return false
-                return False
+  def can_accomodate(self, desired):
+    for d in desired:
+      if d not in self.preferences:
+        # as long as one strict requirement cannot be met, return false
+         return False
+    # otherwise all checks pass, return True
+    return True
 
-        # otherwise all checks pass, return True
-        return True
-
-    @classmethod
-    def fromJSON(cls, data):
-        r = Restaurant(data["name"], [])
-        return r
+  @classmethod
+  def fromJSON(cls, data):
+    r = Restaurant(data["name"], [])
+    return r
 
 class DietaryPreference:
-    """
+  """
     is_strict: used to indicate whether the restriction is strict or not.
                When applied to a person, indicates whether the preference is
                actually a restriction, for example:
@@ -37,56 +36,58 @@ class DietaryPreference:
     inverse:   See examples above, but it inverts the choice, so:
 
                DietaryPreference("thai", False, False) -> Prefer thai
-               DietaryPreference("thai", False, True) -> Prefer not thai
-               DietaryPreference("thai", True True) -> Cannot have thai(loser)
+               DietaryPreference("thai", False, True) -> Prefer not thai DietaryPreference("thai", True True) -> Cannot have thai(loser)
     """
-    def __init__(self, name, is_strict, inverse=False):
-        self.name = name
-        self.is_strict = is_strict
-        self.inverse = inverse
+  def __init__(self, name, is_strict, inverse=False):
+    self.name = name
+    self.is_strict = is_strict
+    self.inverse = inverse
 
-    def __eq__(self, other):
-        return other.name.lower() == self.name.lower()
+  def __eq__(self, other):
+    return other.name.lower() == self.name.lower()
 
 class Person:
-    def __init__(self, canonical_name, preferences=[]):
-        self.canonical_name = canonical_name
-        self.preferences = preferences
+  def __init__(self, canonical_name, preferences=[]):
+    self.canonical_name = canonical_name
+    self.preferences = preferences
 
-    def matches(self, name):
-        #for now, just lowercase and do any sort of partial string compare
-        return self.canonical_name.lower() == name.lower()
+  def matches(self, name):
+    # for now, just lowercase and do any sort of partial string compare
+    return self.canonical_name.lower() == name.lower()
 
 def find_person(person_list, name):
-    for p in person_list:
-        if p.matches(name):
-            return p
+  for p in person_list:
+    if p.matches(name):
+      return p
 
 
 class GetALunchSpotIntent:
   def __init__(self, alexa_intent):
-      self.people = alexa_intent.get_slot_map()["Person"].split()
-      self.people = [p for p in filter(lambda s: s not in ["and", "everyone", "team", "everybody"], self.people)]
-      self.people = map(lambda name: find_person(persons, name), self.people)
+    slotmap = alexa_intent.get_slot_map()
+    self.people = ""
+    if slotmap.has_key("Person"):
+      self.people = slotmap["Person"].split()
+    self.people = [p for p in filter(lambda s: s not in ["and", "everyone", "team", "everybody"], self.people)]
+    self.people = map(lambda name: find_person(persons, name), self.people)
 
 class GetALunchSpotResponse:
-    restaurant = None
+  restaurant = None
 
-    def __init__(self, alexa_intent):
-        self.intent = GetALunchSpotIntent(alexa_intent)
-        self.restaurant = restaurants[0]
+  def __init__(self, alexa_intent):
+    self.intent = GetALunchSpotIntent(alexa_intent)
+    self.restaurant = restaurants[0]
 
-    def find_restaurant(self):
-        preferences = [prefs for person in self.intent.people for prefs in person.preferences]
+  def find_restaurant(self):
+    preferences = [prefs for person in self.intent.people for prefs in person.preferences]
 
-        # always accomodate strict preferences
-        strict = filter(lambda pref: pref.is_strict, preferences)
+    # always accomodate strict preferences
+    strict = filter(lambda pref: pref.is_strict, preferences)
 
-        # find all restaurants that match the strict criteria
-        eligible = filter(lambda r: r.can_accomodate(strict), restaurants)
-        pprint([r.name for r in eligible])
+    # find all restaurants that match the strict criteria
+    eligible = filter(lambda r: r.can_accomodate(strict), restaurants)
+    pprint([r.name for r in eligible])
 
-        return eligible[randrange(0, len(eligible))]
+    return eligible[randrange(0, len(eligible))]
 
 
 restaurants_string = """[{
@@ -95,20 +96,20 @@ restaurants_string = """[{
 restaurants_json = json.loads(restaurants_string)
 
 restaurants = [
-Restaurant("Sweetgreen", [DietaryPreference("vegan", False), DietaryPreference("vegetarian", False), DietaryPreference("gluten-free", False)]),
-Restaurant("Thai Villa", [DietaryPreference("vegan", False), DietaryPreference("vegetarian", False), DietaryPreference("gluten-free", False)]),
-Restaurant("Ribalta", []),
-Restaurant("Vapiano", []),
-Restaurant("Friend of a Farmer", [])
-]
+    Restaurant("Sweetgreen", [DietaryPreference("vegan", False), DietaryPreference("vegetarian", False), DietaryPreference("gluten-free", False)]),
+    Restaurant("Thai Villa", [DietaryPreference("vegan", False), DietaryPreference("vegetarian", False), DietaryPreference("gluten-free", False)]),
+    Restaurant("Ribalta", []),
+    Restaurant("Vapiano", []),
+    Restaurant("Friend of a Farmer", [])
+    ]
 
 persons = [
-Person("Colden", []),
-Person("Cara", [DietaryPreference("gluten-free", True)]),
-Person("Helen", [DietaryPreference("gluten-free", True), DietaryPreference("vegan", True)]),
-Person("Justine", [DietaryPreference("gluten-free", True), DietaryPreference("vegan", True)]),
-Person("Rads", [DietaryPreference("vegetarian", True)]),
-Person("David", [DietaryPreference("meat", False)]),
-Person("Ying"),
-Person("Tom")
-]
+     Person("Colden", []),
+    Person("Cara", [DietaryPreference("gluten-free", True)]),
+    Person("Helen", [DietaryPreference("gluten-free", True), DietaryPreference("vegan", True)]),
+    Person("Justine", [DietaryPreference("gluten-free", True), DietaryPreference("vegan", True)]),
+    Person("Rads", [DietaryPreference("vegetarian", True)]),
+    Person("David", [DietaryPreference("meat", False)]),
+    Person("Ying"),
+    Person("Tom")
+    ]
